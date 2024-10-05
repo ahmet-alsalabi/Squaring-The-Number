c2e6e4f2e8f970bd59ce88033ce57f207a3c5306
ahmet-alsalabi <ahmetalsalabi@gmail.com>
2024-10-05 15:05:34 +0300

    switch the function to arrow function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function square() {
+const square = () => {
     let x = document.getElementById("num-Inp").value;
     let toplam = x * x; 
     document.getElementById("total").textContent = toplam;
